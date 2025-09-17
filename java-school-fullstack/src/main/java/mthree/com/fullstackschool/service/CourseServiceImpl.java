e8cfdf1fe9f008b54016da22cec9b73562bfe6b7
egarne <efthymiagkarneta@gmail.com>
2025-09-17 16:27:49 +0300

    Commit2

--- java-school-fullstack/src/main/java/mthree/com/fullstackschool/service/CourseServiceImpl.java
+++ java-school-fullstack/src/main/java/mthree/com/fullstackschool/service/CourseServiceImpl.java
@@ -1,6 +1,7 @@
 package mthree.com.fullstackschool.service;
 
 import mthree.com.fullstackschool.dao.CourseDao;
+import mthree.com.fullstackschool.dao.CourseDaoImpl;
 import mthree.com.fullstackschool.model.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -12,14 +13,19 @@ public class CourseServiceImpl implements CourseServiceInterface {
 
     //YOUR CODE STARTS HERE
 
+    private final CourseDao courseDao;
 
+    public CourseServiceImpl(CourseDao courseDao) {
+        this.courseDao = courseDao;
+    }
 
     //YOUR CODE ENDS HERE
 
     public List<Course> getAllCourses() {
         //YOUR CODE STARTS HERE
 
-        return null;
+        List<Course> courses = courseDao.getAllCourses();
+        return courses;
 
         //YOUR CODE ENDS HERE
     }
@@ -27,7 +33,8 @@ public class CourseServiceImpl implements CourseServiceInterface {
     public Course getCourseById(int id) {
         //YOUR CODE STARTS HERE
 
-        return null;
+        Course course = courseDao.findCourseById(id);
+        return course;
 
         //YOUR CODE ENDS HERE
     }
@@ -35,7 +42,12 @@ public class CourseServiceImpl implements CourseServiceInterface {
     public Course addNewCourse(Course course) {
         //YOUR CODE STARTS HERE
 
-        return null;
+        if(courseDao.findCourseById(course.getCourseId()) != null) {
+            throw new IllegalArgumentException("ERROR: Could not create new because Course with Id: "
+                    + course.getCourseId() + " already exists.");
+        }
+        Course newCourse = courseDao.createNewCourse(course);
+        return newCourse;
 
         //YOUR CODE ENDS HERE
     }
@@ -43,7 +55,14 @@ public class CourseServiceImpl implements CourseServiceInterface {
     public Course updateCourseData(int id, Course course) {
         //YOUR CODE STARTS HERE
 
-        return null;
+        Course existingCourse = courseDao.findCourseById(id);
+        if(existingCourse == null) {
+            throw new IllegalArgumentException("ERROR: Could not update because Course with Id: "
+                    + id + " does not exist.");
+        }
+        course.setCourseId(id);
+        courseDao.updateCourse(course);
+        return course;
 
         //YOUR CODE ENDS HERE
     }
@@ -51,7 +70,13 @@ public class CourseServiceImpl implements CourseServiceInterface {
     public void deleteCourseById(int id) {
         //YOUR CODE STARTS HERE
 
-
+        Course removedCourse = courseDao.findCourseById(id);
+        if(removedCourse == null) {
+            throw new IllegalArgumentException("ERROR: Could not delete because Course with Id: "
+                    + id + " does not exist.");
+        }
+        courseDao.deleteAllStudentsFromCourse(id);
+        courseDao.deleteCourse(id);
 
         //YOUR CODE ENDS HERE
     }
